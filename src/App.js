68ded0f9e53c16f133ef7804f0676eac0d6044bc
Krishnakalani111 <Krishnakalani7@gmail.com>
2023-02-04 14:06:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [Horizontal, setHorizontal] = useState(10);
+  const [Vertical, setVertical] = useState(10);
+  const [Blur, setBlur] = useState(10);
+  const [Color, setColor] = useState(10);
+  
+
+
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div className="input">
+        <div className="header">
+          <h1>BOX-SHADOW-GENERATOR</h1>
+        </div>
+        <label className="labelss">Horizontal box shadow</label>
+        <input type="range" value={Horizontal} onChange={(e) => { setHorizontal(e.target.value) } } className="inputt" min="-200" max="200" />
+      
+        <label className="labelss">Vertical box shadow</label>
+        <input type="range" value={Vertical} onChange={(e) => { setVertical(e.target.value) }} className="inputt" min="-200" max="200" />
+        
+        <label className="labelss">Blur amount</label>
+        <input type="range" value={Blur} onChange={(e) => { setBlur(e.target.value) }} className="inputt" min="-200" max="200" />
+        
+        <label className="labelss">color:</label>
+        <input type="color" default={{color:"red"}} value={Color} onChange={(e) => { setColor(e.target.value) } } className="inputt" min="-200" max="200" />
+      
+      
+      </div>
+      <div className="output" style={{boxShadow:`${Horizontal}px ${Vertical}px ${Blur}px ${Color} `}}>
+        <div className="text" >{`box-shadow:${Horizontal}px ${Vertical}px ${Blur}px ${Color} `}</div>  
+        
+      </div>
     </div>
   );
 }
